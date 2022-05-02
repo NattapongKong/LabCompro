35b754c8764bfcd9c38f5a7f8fb156bad8babfac
Nattapong_Kongwattananukulkit <hhnatt25@gmail.com>
2022-05-02 18:25:58 +0700

    update

--- quiz3/compiler.c
+++ quiz3/compiler.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-int main(){
-
-    printf("%d" , 8*2-10%4);
+struct t
+{
+    int code;
+    int amount;
+};
+int main()
+{
+    int number[3] = {23, -186, 43};                    // [a]
+    float value[5] = {0.28, 23.456, 3.45, 8.62, 0.96}; // [b]
+    char place[5] = "KMITL";                           // [c]
+    char name[9] = {'E', 'n', 'g', 'i', 'n', 'e', 'e', 'r'}; // [d
+    char sentence[] = "Programming is fun."; // [e]
 }
--- quiz4/quiz4-3.c
+++ quiz4/quiz4-3.c
@@ -1,32 +1,35 @@
-//write the pyramid pattern program in c
-//     *      
-//    ***     
-//   *****    
-//  *******   
-// ********* 
+// write the pyramid pattern program in c
+//      *
+//     ***
+//    *****
+//   *******
+//  *********
 #include <stdio.h>
 int main()
 {
-    int i,j,num;
+    int i, j, num;
     printf(" *** Show isosceles triangle *** \n");
     printf("Enter a counting number : ");
-    scanf("%d",&num);
-    
-    if(num > 3){
+    scanf("%d", &num);
+
+    if (num > 3)
+    {
         printf("Output : \n");
-         for(i=1;i<=num;i++){
-             for(j=1;j<=num-i;j++)
-             {
-                 printf(" ");
-             }
-             for(j=1;j<=2*i-1;j++)
-             {
-                 printf("*");
-             }
-             printf("\n");
-         }
+        for (i = 1; i <= num; i++)
+        {
+            for (j = 1; j <= num - i; j++)
+            {
+                printf(" ");
+            }
+            for (j = 1; j <= 2 * i - 1; j++)
+            {
+                printf("*");
+            }
+            printf("\n");
+        }
     }
-    else{
+    else
+    {
         printf(" --- Incorrect number. --- ");
     }
     return 0;
